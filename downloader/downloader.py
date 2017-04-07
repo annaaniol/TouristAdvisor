1e756dd63287b0f9484ad9d570a846ced5721977
annaaniol <aa.annaaniol@gmail.com>
2017-04-07 18:37:18 +0200

    location data downloader added

---
+++ downloader/downloader.py
@@ -0,0 +1,45 @@
+import requests
+import sys
+from geopy.geocoders import Nominatim
+
+class Downloader:
+
+    def generateCoordinatesList(city):
+        api_key="ca830d075b22ef03d4556f9405fa17ad"
+
+        def getCoordinates(city):
+            geolocator = Nominatim()
+            location = geolocator.geocode(city)
+            print('Found city: '+location.address)
+            return (location.latitude, location.longitude)
+
+        def getPhotoIDs(lat, lon):
+            URL = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key={0}" \
+                  "&accuracy=11&has_geo&lat={1}&lon={2}&format=json&nojsoncallback=1".format(api_key, lat, lon)
+            Response = requests.get(URL).json()
+            PhotoIDs = []
+
+            for photos in Response['photos']['photo']:
+                PhotoIDs.append(photos['id'])
+            return PhotoIDs
+
+        def getLocations(IDs):
+            f = open(str("location_data_"+city),'w')
+            Locations = []
+            for ID in IDs:
+                URL = "https://api.flickr.com/services/rest/?method=flickr.photos.geo.getLocation" \
+                      "&api_key=ca830d075b22ef03d4556f9405fa17ad&photo_id={0}&format=json&nojsoncallback=1".format(ID)
+                Response = requests.get(URL).json()
+                lat = Response['photo']['location']['latitude']
+                lon = Response['photo']['location']['longitude']
+                f.write(lat+' '+lon+'\n')
+                Locations.append((lat,lon))
+            f.close()
+            return Locations
+
+        (latitude,longitude) = getCoordinates(city)
+        PhotoIDs = getPhotoIDs(latitude, longitude)
+        Locations = getLocations(PhotoIDs)
+        return Locations
+
+Downloader.generateCoordinatesList(city = sys.argv[1])
