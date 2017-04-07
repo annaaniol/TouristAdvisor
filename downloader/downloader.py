41342df1632c678cc37aa22c471d173eca60d501
annaaniol <aa.annaaniol@gmail.com>
2017-04-08 00:01:51 +0200

    downloader refactor

--- downloader/downloader.py
+++ downloader/downloader.py
@@ -11,19 +11,31 @@ class Downloader:
         self.points_location = []
         self.photo_ids = []
         self.coordinates = []
+        self.max_pages = 30
 
     def get_city_coordinates(self):
         geolocator = Nominatim()
         self.city_location = geolocator.geocode(self.city_name)
 
-    def get_photo_ids(self, latitude, longitude):
-        URL = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key={0}" \
-              "&accuracy=11&has_geo&lat={1}&lon={2}&format=json&nojsoncallback=1".\
-            format(self.api_key, self.city_location.latitude, self.city_location.longitude)
+    def get_photo_ids_single_page(self, page_number):
+        URL = "https://api.flickr.com/services/rest/?method=flickr.photos.search" \
+              "&api_key={0}&accuracy=11&min_upload_date=2016-01-01&has_geo=1" \
+              "&lat={1}&lon={2}&format=json&nojsoncallback=1&per_page=200&page={3}". \
+            format(self.api_key, self.city_location.latitude, self.city_location.longitude, page_number)
         Response = requests.get(URL).json()
-
         for photos in Response['photos']['photo']:
             self.photo_ids.append(photos['id'])
+
+    def get_photo_ids(self, latitude, longitude):
+        URL = "https://api.flickr.com/services/rest/?method=flickr.photos.search" \
+              "&api_key={0}&accuracy=11&min_upload_date=2016-01-01&has_geo=1" \
+              "&lat={1}&lon={2}&format=json&nojsoncallback=1&per_page=200". \
+            format(self.api_key, self.city_location.latitude, self.city_location.longitude)
+        Response = requests.get(URL).json()
+        pages_total = Response['photos']['pages']
+        pages_total = min(pages_total, self.max_pages)
+        for current_page_number in range(1, pages_total+1):
+            self.get_photo_ids_single_page(current_page_number)
 
     def get_locations(self):
         for ID in self.photo_ids:
@@ -36,14 +48,13 @@ class Downloader:
 
     def write_to_csv(self):
         for cord_tuple in self.coordinates:
-            print (cord_tuple[0] + "," + cord_tuple[1])
+            print(cord_tuple[0] + "," + cord_tuple[1])
 
     def get_points(self):
         self.get_city_coordinates()
         self.get_photo_ids(self.city_location.latitude, self.city_location.longitude)
         self.get_locations()
         self.write_to_csv()
-
 
 downloader = Downloader("Krakow")
 downloader.get_points()
