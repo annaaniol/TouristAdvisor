b38bf1bfad22c2bbf69cda25668653c44473471c
gborkowski <gb.grzegorzborkowski@gmail.com>
2017-04-09 16:14:40 +0200

    macierz kutasiku

--- clustering/cluster.py
+++ clustering/cluster.py
@@ -1,16 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.cluster import DBSCAN
+from geopy.distance import vincenty
+from collections import defaultdict
 
-data = np.loadtxt(fname="data.txt", delimiter=",")
-print(data.shape)
-# X = data[:, :1]
-# Y = data[:, 1:2]
-# print(X, Y)
-# plt.scatter(X, Y)
-# plt.show()
+data = np.loadtxt(fname="../downloader/new_data.txt", delimiter=",")
 
-db = DBSCAN(eps = 0.01, min_samples=10).fit(data)
+db = DBSCAN(eps = 0.001, min_samples=5).fit(data)
 core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
 labels = db.labels_
 n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
@@ -33,4 +29,28 @@ for k, col in zip(unique_labels, colors):
 
 plt.title('Estimated number of clusters: %d' % n_clusters_)
 plt.show()
+points = zip(data, labels)
 
+points = zip(data, labels)
+grouped = defaultdict(list)
+for value, key in points:
+    grouped[key].append(value)
+
+centers = dict()
+for label in grouped:
+    values = (grouped.get(label))
+    sum_x = 0
+    sum_y = 0
+    for point in values:
+        sum_x = sum_x + point[0]
+        sum_y = sum_y + point[1]
+    centers[label] = ((sum_x / len(values), sum_y / len(values)), len(values))
+
+centers.pop(-1)
+distances = np.zeros((len(unique_labels)-1, len(unique_labels)-1))
+
+for first in range(len(distances)):
+    for second in range(len(distances)):
+        distances[first, second] = vincenty(centers.get(first)[0], centers.get(second)[0]).meters
+
+print(distances)
